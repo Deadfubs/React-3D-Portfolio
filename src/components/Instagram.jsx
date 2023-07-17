9df4b2c97040db4ad72b1f8b186fb63acc7b93f1
Fúlvio <deadfubs@gmail.com>
2023-07-17 14:38:37 -0300

    adjusting logo rotation

--- src/components/Instagram.jsx
+++ src/components/Instagram.jsx
@@ -14,8 +14,10 @@ export default function Model(props) {
   const { nodes, materials } = useGLTF('/instagram-transformed.glb')
   return (
     <group {...props} dispose={null}>
-      <mesh geometry={nodes.Object_4.geometry} material={materials.material} position={[0, 0.102, 0.053]} rotation={[-2.884, 0, -Math.PI / 2]} scale={[1, 0.092, 1]} />
-      <mesh geometry={nodes.Object_6.geometry} material={materials['Material.002']} position={[0.12, 0.104, 0.052]} rotation={[0, 0, -Math.PI / 2]} scale={[0.296, 0.037, 0.296]} />
+      <group position={[0, 2.96, -0.13]} rotation={[Math.PI / 2, 0, 1.6]}>
+        <mesh geometry={nodes.Object_4.geometry} material={materials.material} position={[0, 0.102, 0.053]} rotation={[-2.884, 0, -Math.PI / 2]} scale={[1, 0.092, 1]} />
+        <mesh geometry={nodes.Object_6.geometry} material={materials['Material.002']} position={[0.12, 0.104, 0.052]} rotation={[0, 0, -Math.PI / 2]} scale={[0.296, 0.037, 0.296]} />
+      </group>
     </group>
   )
 }
